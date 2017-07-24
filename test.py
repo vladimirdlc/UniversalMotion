d223b06635ed3e102b24bf2393afd70c1a122ccf
Vladimir de la Cruz <vladimirdlc@gmail.com>
2017-07-24 16:44:39 -0400

    Outputing results
    Refactoring and futher processings

--- test.py
+++ test.py
@@ -9,6 +9,11 @@ from keras.layers import Input, Dense
 from keras.models import Model
 from numpy import array
 
+import math
+
+def sigmoid(x):
+  return 1 / (1 + math.exp(-x))
+
 def writeFrame(file, node, i):
         if  node.position:
             for pos in node.position[i]:
@@ -36,6 +41,7 @@ def getFullFrame(node, i, fullFrame=[]):
 def processBVH(fullPath):
         #test = bvh.reader.BvhReader(mypath+onlyfiles[0])
         #test.read();
+        print("Processing... "+fullPath)
         file = open(fullPath, 'r')
 
         data = []
@@ -62,26 +68,47 @@ def processBVH(fullPath):
             
             for x,y,z in zip(*[iter(floats)]*3):
                 quat = Quat((x,y,z))
-                quaternionData.extend((quat.q+1)/2) #normalize the data
+                quaternionData.append((quat.q+1)/2)
+                #quaternionData.append((sigmoid(x),sigmoid(y),sigmoid(z))) #normalize the data, extend?
 
             framesQData.append(quaternionData)
 
-        print(len(framesQData))
-        print(len(framesQData[0]))
+        #print(len(framesQData))
+        #print(len(framesQData[0]))
         #writeFrame(file, test.root, 0)
         #file.write("\n")
         file.close()
         
         return framesQData
 
-encoding_dim = 120  # 88 floats -> compression of factor 50%, assuming the input is 176 floats
+mypath = 'data/CMU/testing/'
+onlyfolders = [f for f in listdir(mypath) if not isfile(join(mypath, f))]
+
+qdata = []
+
+for folder in onlyfolders:
+    onlyfiles = [f for f in listdir(mypath+folder) if isfile(join(mypath+folder, f))]
+    for filename in onlyfiles:
+        qdata.extend(processBVH(mypath+folder+'/'+filename))
+
+dataSplitPoint = int(len(qdata)*0.2)
+trainingData = array(qdata[dataSplitPoint:len(qdata)]).astype('float32')
+validationData = array(qdata[0:dataSplitPoint]).astype('float32')
+
+#vdata = tdata
+
+trainingData = trainingData.reshape((len(trainingData), np.prod(trainingData.shape[1:])))
+validationData = validationData.reshape((len(validationData), np.prod(validationData.shape[1:])))
+
+input_size = len(trainingData[0])
+encoding_dim = int(input_size*0.1) # compression of factor 20%, assuming the input is nn floats
 
 # this is our input placeholder
-input_frame = Input(shape=(44, 4, 1))
+input_frame = Input(shape=(input_size,))
 # "encoded" is the encoded representation of the input
 encoded = Dense(encoding_dim, activation='relu')(input_frame)
 # "decoded" is the lossy reconstruction of the input
-decoded = Dense(176, activation='sigmoid')(encoded)
+decoded = Dense(input_size, activation='sigmoid')(encoded)
 
 # this model maps an input to its reconstruction
 autoencoder = Model(input_frame, decoded)
@@ -99,42 +126,25 @@ decoder = Model(encoded_input, decoder_layer(encoded_input))
 
 autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
 
-mypath = 'data/CMU/testing/'
-onlyfolders = [f for f in listdir(mypath) if not isfile(join(mypath, f))]
-
-qdata = []
-
-for folder in onlyfolders:
-    onlyfiles = [f for f in listdir(mypath+folder) if isfile(join(mypath+folder, f))]
-    for filename in onlyfiles:
-        qdata.append(processBVH(mypath+folder+'/'+filename))
-
-dataSplitPoint = int(len(qdata)*0.2)
-trainingData = array(qdata[dataSplitPoint:len(qdata)])
-validationData = array(qdata[0:dataSplitPoint])
-
-#trainingd = []
-#for tdata in trainingData:
-#    trainingd.extend(tdata)
-
-#validationd = []
-#for vdata in validationData:
-#    validationd.extend(vdata)
-
-#vdata = tdata
-    
-#x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
-#trainingData = trainingData.reshape((len(trainingData), np.prod(trainingData.shape[1:])))
-#validationData = validationData.reshape((len(validationData), np.prod(validationData.shape[1:])))
-
 autoencoder.fit(trainingData, trainingData,
-                epochs=50,
+                epochs=200,
                 batch_size=256,
                 shuffle=False,
                 validation_data=(validationData, validationData))
-                
-print(len(trainingData))
-print(len(validationData))
+
+encoded_quat = encoder.predict(trainingData)
+decoded_quat = decoder.predict(encoded_quat)
+
+file = open(mypath+'test.txt', 'w')
+
+for frame in decoded_quat:
+    for frameData in zip(*[iter(frame)]*input_size):
+        file.write('\n'+str(frameData))
+
+file.close()
+
+#print(len(trainingData))
+#print(len(validationData))
 
 #(mypath+filename[1])
 #getFullFrame(file, test.root, 0)
