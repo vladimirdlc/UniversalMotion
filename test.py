88088a358709947fcc9404cbd003577dfa761427
Vladimir de la Cruz <vladimirdlc@gmail.com>
2017-07-24 00:58:33 -0400

    Added autoencoder code

--- test.py
+++ test.py
@@ -5,6 +5,10 @@ import bvh.helpers
 from os import listdir
 from os.path import isfile, join
 
+from keras.layers import Input, Dense
+from keras.models import Model
+from numpy import array
+
 def writeFrame(file, node, i):
         if  node.position:
             for pos in node.position[i]:
@@ -52,10 +56,10 @@ def processBVH(fullPath):
         framesQData = []
 
         for currentFrame in data:
-
             quaternionData = []
             floats = [float(x) for x in currentFrame.split()]
-            print(floats)
+            #print(floats)
+            
             for x,y,z in zip(*[iter(floats)]*3):
                 quat = Quat((x,y,z))
                 quaternionData.extend((quat.q+1)/2) #normalize the data
@@ -67,13 +71,71 @@ def processBVH(fullPath):
         #writeFrame(file, test.root, 0)
         #file.write("\n")
         file.close()
+        
+        return framesQData
 
+encoding_dim = 80  # 88 floats -> compression of factor 50%, assuming the input is 176 floats
 
-mypath = 'data/CMU/107/'
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+# this is our input placeholder
+input_frame = Input(shape=(176,))
+# "encoded" is the encoded representation of the input
+encoded = Dense(encoding_dim, activation='relu')(input_frame)
+# "decoded" is the lossy reconstruction of the input
+decoded = Dense(176, activation='sigmoid')(encoded)
 
-for filename in onlyfiles:
-    processBVH(mypath+filename)
+# this model maps an input to its reconstruction
+autoencoder = Model(input_frame, decoded)
 
+# this model maps an input to its encoded representation
+encoder = Model(input_frame, encoded)
+
+# create a placeholder for an encoded (176-dimensional) input
+encoded_input = Input(shape=(encoding_dim,))
+# retrieve the last layer of the autoencoder model
+decoder_layer = autoencoder.layers[-1]
+# create the decoder model
+decoder = Model(encoded_input, decoder_layer(encoded_input))
+
+autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+mypath = 'data/CMU/testing/'
+onlyfolders = [f for f in listdir(mypath) if not isfile(join(mypath, f))]
+
+qdata = []
+
+for folder in onlyfolders:
+    onlyfiles = [f for f in listdir(mypath+folder) if isfile(join(mypath+folder, f))]
+    for filename in onlyfiles:
+        qdata.extend(processBVH(mypath+folder+'/'+filename))
+
+dataSplitPoint = int(len(qdata)*0.2)
+trainingData = array(qdata[dataSplitPoint:len(qdata)])
+validationData = array(qdata[0:dataSplitPoint])
+
+#trainingd = []
+#for tdata in trainingData:
+#    trainingd.extend(tdata)
+
+#validationd = []
+#for vdata in validationData:
+#    validationd.extend(vdata)
+
+#vdata = tdata
+    
+#x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+#trainingData = trainingData.reshape((len(trainingData), np.prod(trainingData.shape[1:])))
+#validationData = validationData.reshape((len(validationData), np.prod(validationData.shape[1:])))
+
+autoencoder.fit(trainingData, trainingData,
+                epochs=50,
+                batch_size=256,
+                shuffle=False,
+                validation_data=(validationData, validationData))
+                
+print(len(trainingData))
+print(len(validationData))
+
+#(mypath+filename[1])
 #getFullFrame(file, test.root, 0)
 
+#exec(open("test.py").read())
