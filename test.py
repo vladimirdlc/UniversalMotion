60084b235365cb8c26fc2c6e78406c52c79d31db
Vladimir de la Cruz <vladimirdlc@gmail.com>
2017-10-22 11:01:19 -0400

    New Conv1D/Min-MaxPooling structure

--- test.py
+++ test.py
@@ -6,8 +6,8 @@ import bvh.helpers
 from os import listdir
 from os.path import isfile, join
 
-from keras.layers import Input, Dense
-from keras.models import Model
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, Conv1D, Dropout, MaxPooling1D, UpSampling1D
+from keras.models import Model, Sequential
 from numpy import array
 import numpy as np
 
@@ -15,6 +15,7 @@ import math
 import sys
 
 from itertools import islice
+
 
 def sigmoid(x):
   return 1 / (1 + math.exp(-x))
@@ -53,10 +54,12 @@ def window(seq, n=2):
     for elem in it:
         result = result[1:] + (elem,)
         yield result
-        
+
+
 def processBVH(fullPath):
         #test = bvh.reader.BvhReader(mypath+onlyfiles[0])
         #test.read();
+        
         print("Processing... "+fullPath)
         file = open(fullPath, 'r')
 
@@ -105,7 +108,7 @@ def processBVH(fullPath):
                 #quaternionData.append((x,y,z))
 
             framesQData.append(quaternionData)
-
+            
         #print(len(framesQData))
         #print(len(framesQData[0]))
         #writeFrame(file, test.root, 0)
@@ -128,13 +131,25 @@ qdata = []
 rootPos = []
 rootRot = []
 
+'''
+
+targetFrames = 25
+
+
 for folder in onlyfolders:
     onlyfiles = [f for f in listdir(mypath+folder) if isfile(join(mypath+folder, f))]
     for filename in onlyfiles:
+        currentFrameChunk = 0;
         qFrame, qRootPos, qRootRot = processBVH(mypath+folder+'/'+filename)
-        qdata.extend(qFrame)
-        rootPos.extend(qRootPos)
-        rootRot.extend(qRootRot)
+        
+        for subwindow in window(qFrame, targetFrames): #window
+            qdata.append(subwindow)
+            
+        for subwindow in window(qRootPos, targetFrames):
+            rootPos.append(subwindow)
+        
+        for subwindow in window(qRootRot, targetFrames):
+            rootRot.append(subwindow)
 
 dataSplitPoint = int(len(qdata)*0.8)
 
@@ -142,10 +157,15 @@ trainingData = array(qdata).astype('float32')
 validationData = array(qdata[dataSplitPoint:len(qdata)]).astype('float32')
 trainingData = array(qdata[0:dataSplitPoint]).astype('float32')
 
-n = 2   
+n = 240
 
-trainingData = trainingData.reshape((len(trainingData), np.prod(trainingData.shape[1:])))
-validationData = validationData.reshape((len(validationData), np.prod(validationData.shape[1:])))
+
+#trainingData = trainingData.reshape((len(trainingData), np.prod(trainingData.shape[1:])))
+#validationData = validationData.reshape((len(validationData), np.prod(validationData.shape[1:])))
+
+
+#s[0]
+#trainingData.shape
 
 firstInputSize = len(trainingData[0])
 
@@ -171,42 +191,63 @@ validationData = array(valdata2)
 input_size = len(trainingData[0])
 #encoding_dim = int(input_size*0.5) # compression of factor 20%, assuming the input is nn floats
 
-# this is our input placeholder
-input_frame = Input(shape=(input_size,))
+'''
 
+X = np.load('data_rotation_full_cmu.npz')['clips']
+
+print(X.shape)
+
+#X = np.concatenate([Xcmu, Xhdm05, Xmhad, Xstyletransfer, Xedin_locomotion, Xedin_xsens, Xedin_misc, Xedin_punching], axis=0)
+X = X.reshape(X.shape[0], X.shape[1], X.shape[2]*X.shape[3])
+#X = np.reshape(X, )
+X = np.swapaxes(X, 1, 2).astype('float32')
+
+trainingData = X
+validationData = X
+
+print(X.shape)
+
+network = Sequential()
+#https://keras.io/layers/convolutional/#conv1d
+#(17978, 84, 240)
+degreesOFreedom = X.shape[1] #84
+window = 240
+batch_size = 1
+network.add(Dropout(0.25, input_shape=(degreesOFreedom, window)))
+#3*22 joints + 4 contact timings + 2 root translational velocity + 1 root rotational velocity
+#4*22
+
+batchsize=1
+kernel_size = 25
+network.add(Conv1D(256, kernel_size, use_bias=True, activation='tanh', padding='same'))
+network.add(MaxPooling1D(padding='same'))
+#now the decoder
+#network.add(UpSampling1D(size=2), output_shape=(batchsize, 256, window))
+network.add(UpSampling1D(size=2))
+network.add(Dropout(0.25))
+
+network.add(Conv1D(240, kernel_size, use_bias=True, activation='tanh', padding='same'))
+
+network.summary()
+#(filters, kernel_size, strides=1, padding='valid', dilation_rate=1, activation=None, use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, bias_constraint=None)
+#model.add(Dropout(0.25))
 # "encoded" is the encoded representation of the input
 #encoded = Dense(encoding_dim, activation='relu')(input_frame)
-encoded = Dense(input_size-1, activation='tanh')(input_frame)
-encoded = Dense(input_size-2, activation='tanh')(encoded)
-encoded = Dense(input_size-3, activation='tanh')(encoded)
-encoded = Dense(input_size-2, activation='tanh')(encoded)
-encoded = Dense(input_size-1, activation='tanh')(input_frame)
-#encoded = Dense(int(input_size*0.6), activation='relu')(encoded)
-#encoded = Dense(int(input_size*0.5), activation='relu')(encoded)
-
-#decoded = Dense(int(input_size*0.6), activation='relu')(encoded)
-#decoded = Dense(int(input_size*0.8), activation='relu')(decoded)
-
-# "decoded" is the lossy reconstruction of the input
-decoded = Dense(input_size, activation='tanh')(encoded)
-
-# this model maps an input to its reconstruction
-autoencoder = Model(input_frame, decoded)
-autoencoder.summary()
-
 #autoencoder.compile(optimizer='adadelta', loss='mse')
 #autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy', metrics=['accuracy'])
 #autoencoder.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
 # For a mean squared error regression problem
-autoencoder.compile(optimizer='rmsprop', loss='mse', metrics=['accuracy'])
+network.compile(optimizer='rmsprop', loss='mse', metrics=['accuracy'])
 
-#autoencoder.load_weights('autoencoder_weights.h5')
-
-autoencoder.fit(trainingData, trainingData, verbose=1,
+print(trainingData.shape)
+print(validationData.shape)
+network.fit(trainingData, trainingData, verbose=1,
                 epochs=200,
                 batch_size=50,
                 shuffle=True,
                 validation_data=(validationData, validationData))
+
+network.save_weights('autoencoder_weights.h5')
 
 #trainingData = array(qdata[len(qdata)-3698:len(qdata)]).astype('float32')
 trainingData = array(qdata[0:3690]).astype('float32').flatten() #remember to put a multiple of 15
@@ -268,7 +309,7 @@ for frameData in decoded_quat:
 
 file.close()
 #DO HALF OF ROTATION POSITION AVG TARGET WITH MODEL
-autoencoder.save_weights('autoencoder_weights.h5')
+
 
 #print(len(trainingData))
 #print(len(validationData))
