f2d2826ae3eab8ce8a80e7d2f141425a9fee1e2f
Vladimir de la Cruz <vladimirdlc@gmail.com>
2018-04-01 01:20:05 -0400

    Added results for rotationMatrix training with bz16

--- testRotMat.py
+++ testRotMat.py
@@ -70,10 +70,10 @@ epochs = 600
 
 network.compile(optimizer='adam', loss='mse')
 
-batch_size = 16
+batch_size = 1
 #network.load_weights('cmu_rotations_full_cmu_30_w240_standardized_scaled10000_k15_hu512_vtq2_e400_d0.25_bz1_weigths.h5')
 
-idPrefix = '{}_k{}_hu{}_v{}_e{}_d{}_bz{}_valtest0.2'.format(fileChanged,kernel_size,hiddenUnits, version, epochs, dropoutAmount, batch_size)
+idPrefix = '{}_k{}_hu{}_v{}_e{}_d{}_bz{}_valtest0.2'.format(fileChanged, kernel_size, hiddenUnits, version, epochs, dropoutAmount, batch_size)
 
 plot_losses = PlotLoss(epochs, 'results/'+idPrefix)
 
