f9f8ea8eb35a5f1ded7646c67c1b41a155d43a9f
Vladimir de la Cruz <vladimirdlc@gmail.com>
2018-10-05 00:30:43 -0400

    Added noiser script with example files

---
+++ noisy/add_noise.py
@@ -0,0 +1,42 @@
+import re
+from numpy import *
+from random import uniform
+import argparse as Ap
+
+set_printoptions(suppress=True)
+
+def getArgParser():
+    parser = Ap.ArgumentParser(description='Parameters to add noise')
+
+    parser.add_argument("--file", default="numbers_array.txt", type=str)
+    parser.add_argument("--ofile", default="numbers_array_out.txt", type=str)
+    parser.add_argument("--le", default="-5", type=float)
+    parser.add_argument("--ue", default="5", type=float)
+    parser.add_argument("--llimit", default="-180", type=float)
+    parser.add_argument("--ulimit", default="180", type=float)
+
+    args = parser.parse_args()
+    return args
+
+
+def addNoise(qw):
+    frand = uniform(args.le, args.ue) + qw
+
+    if frand > args.ulimit:
+        return frand - 360
+    elif frand < args.llimit:
+        return frand + 360
+
+    return frand
+
+args = getArgParser()
+data = loadtxt(args.file)
+
+print(data[0][1])
+print(data.shape)
+
+for i in range(0, data.shape[0]):
+    for j in range(3, data.shape[1]):
+        data[i][j] = addNoise(data[i][j])
+
+savetxt(args.ofile, data, fmt='%f')
