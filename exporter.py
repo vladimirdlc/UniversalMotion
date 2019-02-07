04d342037b97688e210445d39db83aa5cbcc746c
Vladimir de la Cruz <vladimirdlc@gmail.com>
2019-02-07 08:23:27 -0500

    Exporter now can process all decoding types in 1 run

--- exporter.py
+++ exporter.py
@@ -1,4 +1,9 @@
+
 import os
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
 import sys
 import numpy as np
 from enum import Enum
@@ -10,7 +15,7 @@ class Decoder(Enum):
     AXIS_ANGLE = 'AxisAngle'
 
 
-sys.path.append('../../motion')
+sys.path.append('motion/')
 import BVH as BVH
 import Animation as Animation
 from Quaternions import Quaternions
@@ -22,7 +27,7 @@ wdw = 240
 step = 120
 scale = 1
 
-decodeType = Decoder.ROTATION_MATRIX
+allDecodes = [Decoder.QUATERNION, Decoder.ROTATION_MATRIX]
 
 def process_file_rotations(filename, window=240, window_step=120):
     anim, names, frametime = BVH.load(filename, order='zyx')
@@ -113,28 +118,27 @@ def get_files(directory):
             if os.path.isfile(os.path.join(directory, f))
             and f.endswith('.bvh') and f != 'rest.bvh']
 
+for decodeType in allDecodes:
+	cmu_files = get_files('data/CMU')
+	cmu_rot_clips = []
 
-print("scale: "+scale)
-cmu_files = get_files('cmu')
-cmu_rot_clips = []
+	wdw = 240
+	step = 120
 
-wdw = 240
-step = 120
+	for i, item in enumerate(cmu_files):
+		print('Processing Rotation %i of %i (%s)' % (i, len(cmu_files), item))
+		clips = process_file_rotations(item, window=wdw, window_step=step)
+		cmu_rot_clips += clips
 
-for i, item in enumerate(cmu_files):
-    print('Processing Rotation %i of %i (%s)' % (i, len(cmu_files), item))
-    clips = process_file_rotations(item, window=wdw, window_step=step)
-    cmu_rot_clips += clips
+	data_clips = np.array(cmu_rot_clips)
+	print(data_clips.shape)
 
-data_clips = np.array(cmu_rot_clips)
-print(data_clips.shape)
+	std = np.std(data_clips)
+	print(std)
+	mean = np.mean(data_clips)
+	data_clips -= mean
+	data_clips /= std
+	np.savez_compressed(
+		'cmu_rotations_{}_cmu_{}_standardized_w{}_ws{}_normalfps_scaled{}'.format(decodeType.value, data_clips.shape[2], wdw, step, scale), clips=data_clips, std=std, mean=mean, scale=scale)
 
-std = np.std(data_clips)
-print(std)
-mean = np.mean(data_clips)
-data_clips -= mean
-data_clips /= std
-np.savez_compressed(
-    'cmu_rotations_{}_cmu_{}_standardized_w{}_ws{}_normalfps_scaled{}'.format(decodeType.value, data_clips.shape[2], wdw, step, scale), clips=data_clips, std=std, mean=mean, scale=scale)
-
-print(scale)
+	print('done')
